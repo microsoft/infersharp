73c892dd03c9ebac3c8c1daddf929fd391bbe116
Matthew Jin <matjin@microsoft.com>
2021-02-22 21:59:06 +0000

    edits

--- Cilsil/Cil/Parsers/RetParser.cs
+++ Cilsil/Cil/Parsers/RetParser.cs
@@ -18,7 +18,11 @@ namespace Cilsil.Cil.Parsers
             switch (instruction.OpCode.Code)
             {
                 case Code.Ret:
+                    Store retInstr;
                     var retType = state.Method.ReturnType.GetElementType();
+                    var retNode = new StatementNode(state.CurrentLocation,
+                                                    StatementNode.StatementNodeKind.ReturnStmt,
+                                                    state.ProcDesc);
                     if (retType == state.Method.Module.TypeSystem.Void)
                     {
                         state.PreviousNode.Successors.Add(state.ProcDesc.ExitNode);
@@ -29,13 +33,39 @@ namespace Cilsil.Cil.Parsers
                         Expression returnVariable = new LvarExpression(
                             new LocalVariable(Identifier.ReturnIdentifier,
                                               state.Method));
-                        var retInstr = new Store(lvalue: returnVariable,
-                                                 rvalue: returnValue,
-                                                 type: Typ.FromTypeReference(retType),
-                                                 location: state.CurrentLocation);
-                        var retNode = new StatementNode(state.CurrentLocation,
-                                                        StatementNode.StatementNodeKind.ReturnStmt,
-                                                        state.ProcDesc);
+
+                        if (returnValue is BinopExpression) 
+                        {
+                            // We see that for the auto-generated method op_Inequality in records, 
+                            // an equality expression is pushed directly onto the stack and
+                            // returned. However, return of an expression is not valid in the SIL -- 
+                            // we must inline a variable store and load of the value prior to
+                            // subsequently returning it.
+                            var inlineReturn = new LocalVariable("inlineReturn", state.Method);
+                            var inlineIdentifier = state.GetIdentifier(Identifier.IdentKind.Normal);
+                            var storeInlineReturn = new Store(new LvarExpression(inlineReturn),
+                                                              returnValue,
+                                                              Typ.FromTypeReference(retType),
+                                                              state.CurrentLocation);
+                            var loadInlineReturn = new Load(
+                                inlineIdentifier,
+                                new LvarExpression(inlineReturn),
+                                Typ.FromTypeReference(retType),
+                                state.CurrentLocation);
+                            retNode.Instructions.Add(storeInlineReturn);
+                            retNode.Instructions.Add(loadInlineReturn);
+                            retInstr = new Store(returnVariable,
+                                                 new VarExpression(inlineIdentifier),
+                                                 Typ.FromTypeReference(retType),
+                                                 state.CurrentLocation);
+                        }
+                        else
+                        {
+                            retInstr = new Store(returnVariable,
+                                                 returnValue,
+                                                 Typ.FromTypeReference(retType),
+                                                 state.CurrentLocation);
+                        }
                         retNode.Instructions.Add(retInstr);
                         retNode.Successors = new List<CfgNode> { state.ProcDesc.ExitNode };
                         RegisterNode(state, retNode);
