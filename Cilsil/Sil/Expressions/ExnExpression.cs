1ccb77d8b9ae09a689370b896ed13bbd76224450
Xiaoyu Liu <lixiaoyu@microsoft.com>
2021-05-29 09:42:35 -0700

    Update ExnExpression.cs

--- Cilsil/Sil/Expressions/ExnExpression.cs
+++ Cilsil/Sil/Expressions/ExnExpression.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 namespace Cilsil.Sil.Expressions
 {
     /// <summary>
-    /// Exception expression.
+    /// An expression representing an exception variable.
     /// </summary>
     [JsonObject]
     public class ExnExpression : Expression
