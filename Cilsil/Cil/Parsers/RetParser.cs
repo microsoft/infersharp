1be630654a301750d63a751514ea39a4400328d1
Matthew Jin <matjin@microsoft.com>
2021-09-23 14:22:36 -0700

    create node for void return

--- Cilsil/Cil/Parsers/RetParser.cs
+++ Cilsil/Cil/Parsers/RetParser.cs
@@ -22,13 +22,18 @@ namespace Cilsil.Cil.Parsers
                     var retType = state.Method.ReturnType.GetElementType();
                     if (retType == state.Method.Module.TypeSystem.Void)
                     {
-                        state.PreviousNode.Successors.Add(state.ProcDesc.ExitNode);
+                        var retNode = new StatementNode(state.CurrentLocation,
+                                                        StatementNode.StatementNodeKind.ReturnStmt,
+                                                        state.ProcDesc);
+                        RegisterNode(state, retNode);
+                        state.PreviousNode.Successors.Add(retNode);
+                        retNode.Successors.Add(state.ProcDesc.ExitNode);
                     }
                     else
                     {
                         var retNode = new StatementNode(state.CurrentLocation,
-                                StatementNode.StatementNodeKind.ReturnStmt,
-                                state.ProcDesc);
+                                                        StatementNode.StatementNodeKind.ReturnStmt,
+                                                        state.ProcDesc);
                         (var returnValue, _) = state.Pop();
                         Expression returnVariable = new LvarExpression(
                             new LocalVariable(Identifier.ReturnIdentifier,
