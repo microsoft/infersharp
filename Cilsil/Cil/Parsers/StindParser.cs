9aaf5df6c0f55aeb0241808d8ae660681ca2327a
Matthew Jin <matjin@microsoft.com>
2022-06-28 12:08:03 -0700

    bug fix

--- Cilsil/Cil/Parsers/StindParser.cs
+++ Cilsil/Cil/Parsers/StindParser.cs
@@ -24,8 +24,6 @@ namespace Cilsil.Cil.Parsers
                 case Code.Stind_R4:
                 case Code.Stind_R8:
                 case Code.Stind_Ref:
-                    Load loadExpressionValue;
-                    Identifier freshIdentifier;
                     Store setExpressionValue;
 
                     (var value, _) = state.Pop();
@@ -33,28 +31,8 @@ namespace Cilsil.Cil.Parsers
 
                     if (pointerType is Address address)
                     {
-                        if (address.AddressType == Address.ReferenceKind.Parameter)
-                        {
-                            freshIdentifier = state.GetIdentifier(Identifier.IdentKind.Normal);
-
-                            loadExpressionValue = new Load(freshIdentifier,
-                                                           address.Value,
-                                                           pointerType.StripPointer(),
-                                                           state.CurrentLocation);
-
-                            setExpressionValue =
-                                new Store(new VarExpression(freshIdentifier),
-                                          value,
-                                          pointerType.StripPointer().StripPointer(),
-                                          state.CurrentLocation);
-
-                            state.PushInstruction(
-                                instruction.Next,
-                                AddMethodBodyInstructionsToCfg(state,
-                                                               loadExpressionValue,
-                                                               setExpressionValue));
-                        }
-                        else if (address.AddressType == Address.ReferenceKind.Field)
+                        if (address.AddressType == Address.ReferenceKind.Parameter ||
+                            address.AddressType == Address.ReferenceKind.Field)
                         {
                             setExpressionValue = new Store(pointerExpression,
                                                            value,
