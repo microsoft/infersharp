6abe36248c38dfde4dfbb77ef4a28ff80710e7cb
David Flores <v-daflores@microsoft.com>
2022-08-19 17:05:55 -0600

    Updating Infer resource leak tests.

--- Examples/Examples/InferResourceLeakTests.cs
+++ Examples/Examples/InferResourceLeakTests.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+
+// 13 reports expected (14 with --pulse-increase-leak-recall flag)
 
 class InferResourceLeakTests
 {
@@ -23,44 +28,53 @@ class InferResourceLeakTests
         }
     }
 
-    public static void ConstructionCanThrowShouldReport()
+    public static void ThrowBetweenConstructionAndDisposeShouldReport()
     {
-        // bad because stream2 could throw, preventing stream 1 from closing
-        var stream1 = new FileStream("NotAFile.bad", FileMode.Open);
-        var stream2 = new FileStream("NotAFile.bad", FileMode.Append);
-
-        stream1.Dispose();
-        stream2.Dispose();
-    }
-
-    public static void ConstructingOnePotentialThrowOk()
-    {
-        // if new FileStream throw, the resource should not be allocated and does not need to be disposed?
-        var stream1 = new FileStream("NotAFile.bad", FileMode.Open);
+        var stream1 = new FileStream("MyFile.txt", FileMode.Open);
+        throw new FileNotFoundException();
         stream1.Dispose();
     }
 
-    public static void DelegatingToThrowingConstructorShouldReport()
+    public static void CallThrowingMethodBetweenConstructionAndDisposeShouldReport()
+    { // this doesn't report since the fact that doesThrow() doesn't get propageted here.
+        var stream1 = new FileStream("MyFile.txt", FileMode.Open);
+        doesThrow();
+        stream1.Dispose();
+    }
+
+    public static void CallThrowingMethodBetweenConstructionAndDisposeShouldReport2()
+    {
+        var stream1 = new FileStream("MyFile.txt", FileMode.Open);
+        doesThrowOrDispose(true, stream1);
+    }
+
+    public static void doesThrow()
+    {
+        throw new ArgumentException("Hi");
+    }
+
+
+    public static void doesThrowOrDispose(bool b, Stream s)
+    {
+        if (b)
+        {
+            throw new ArgumentException("Hi");
+        }
+
+        s.Dispose();
+    }
+    public static void ConstructingOneOk()
+    {
+        var stream1 = new FileStream("NotAFile.bad", FileMode.Open);
+        stream1.Dispose();
+    }
+
+    public static void DelegatingToConstructorOk()
     { 
         var stream1 = new FileStream("SomeFile.txt", FileMode.Open);
-        // if new StreamReader throws, then stream1 has no place to close.
         var stream2 = new StreamReader(stream1);
 
         stream2.Dispose();
-    }
-
-    public static void DelegatingResourceToResourceOk()
-    { // rework
-        // this filestream is managed by the bufferedstream and disposed with it
-        var fs = new FileStream("SomeFile.txt", FileMode.Open);
-        try
-        {
-            var sr = new StreamReader(fs);
-            sr.Dispose();
-        } catch (ArgumentException e)
-        {
-            fs.Dispose();
-        }
     }
 
     public static void DelegatingResourceToResourceShouldReport()
@@ -69,15 +83,8 @@ class InferResourceLeakTests
         var fs = new FileStream("SomeFile.txt", FileMode.Open);
         // this filestream is the stream delegated to the bufferedstream and disposed with it
         fs = new FileStream("SomeFile.txt", FileMode.Open);
-        try
-        {
-            var sr = new StreamReader(fs);
-            sr.Dispose();
-        }
-        catch (ArgumentException e)
-        {
-            fs.Dispose();
-        }
+        var sr = new StreamReader(fs);
+        sr.Dispose();
     }
 
     public static void UsagesNotTrackedOk ()
@@ -98,8 +105,6 @@ class InferResourceLeakTests
     public static void UsageNotTrackedDelegatedOk()
     {
         var stream1 = new MemoryStream();
-        // if new StreamReader throws, then stream1 has no place to close.
-        // but stream1 is a MemoryStream, so that's not a problem.
         var streamReader = new StreamReader(stream1);
 
         streamReader.Dispose();
@@ -108,12 +113,11 @@ class InferResourceLeakTests
     public static void UsageNotTrackedDelegatedShouldReport ()
     {
         var stream1 = new MemoryStream();
-        // if new StreamReader throws, then stream1 has no place to close.
-        // but stream1 is a MemoryStream, so that's not a problem.
         var streamReader = new StreamReader(stream1);
 
         // potential exception here means the stream reader may not be disposed.
         var fs = new FileStream("MyFile.txt", FileMode.Open);
+        fs.Write(myBytes);
         fs.Dispose();
 
         streamReader.Dispose();
@@ -130,26 +134,10 @@ class InferResourceLeakTests
         var fs = CreateStreamOk();
     }
 
-    public static StreamReader CreateStreamReaderLeakInnerShouldReport()
-    { // if the stream reader throws an exception, the fs is leaked
+    public static StreamReader CreateStreamReaderAndReturnOk()
+    {
         var fs = new FileStream("SomeFile.txt", FileMode.Append);
         var sr = new StreamReader(fs);
-        return sr;
-    }
-
-
-    public static StreamReader CreateStreamReaderLeakInnerOk ()
-    { // if the stream reader throws an exception, the fs is leaked
-        var fs = new FileStream("SomeFile.txt", FileMode.Append);
-        StreamReader sr = null;
-        try
-        {
-            sr = new StreamReader(fs);            
-        } catch (ArgumentException e)
-        {
-            fs.Close ();
-            throw;
-        }
         return sr;
     }
 
@@ -158,27 +146,320 @@ class InferResourceLeakTests
         return new FileStream("MyFile.txt", FileMode.Create);
     }
 
-    public static Object LoseIDisposableShouldReport ()
+    public static TakeAndDispose PassDisposableToCustomDisposableOk ()
     {
-        return new InheritTwoDisposable() as MyFavoriteClass;
+        Stream stream = new FileStream ("MyFile", FileMode.Open);
+        return new TakeAndDispose (stream);
+    }
+
+    public static void PassDisposableToCustomDisposableAndDisposeOk()
+    {
+        Stream stream = new FileStream("MyFile", FileMode.Open);
+        TakeAndDispose tad = new TakeAndDispose(stream);
+        tad.Dispose();
+    }
+
+    public static void PassDisposableToCustomDisposableAndDisposeShouldReport()
+    {
+        Stream stream = new FileStream("MyFile", FileMode.Open);
+        TakeAndDispose tad = new TakeAndDispose(stream);
+        stream.Dispose();
+    }
+
+    public static TakeWithoutDispose PassDisposableToCustomDisposable2Ok()
+    {
+        Stream stream = new FileStream("MyFile", FileMode.Open);
+        return new TakeWithoutDispose(stream);
+    }
+
+    public static void PassDisposableToCustomDisposableAndDispose2ShouldReport()
+    {
+        Stream stream = new FileStream("MyFile", FileMode.Open);
+        TakeWithoutDispose twd = new TakeWithoutDispose(stream);
+        twd.Dispose();
+    }
+
+    public static void PassDisposableToCustomClassWithDisposeAndDisposeOk()
+    {
+        Stream stream = new FileStream("MyFile", FileMode.Open);
+        var tadnd = new TakeAndDisposeNotDisposable(stream);
+        tadnd.Dispose();
+    }
+
+    public static TakeAndDisposeNotDisposable PassDisposableToCustomClassWithDisposeOk()
+    {
+        Stream stream = new FileStream("MyFile", FileMode.Open);
+        return new TakeAndDisposeNotDisposable(stream);
+    }
+
+    public static IDisposable CastingOk()
+    {
+        FileStream myStream = new FileStream("MyFile", FileMode.Open);
+
+        return myStream;
+    }
+
+
+    // out variables
+    public static void OutStreamOk(out FileStream myStream)
+    {
+        myStream = new FileStream("myFile.txt", FileMode.OpenOrCreate);
+    }
+
+    // unknown function behavior test
+    public static void UnknownCallIsNopReport()
+    { // reports if the unknown functions do *not* havoc arguments.
+        FileStream myStream = new FileStream("MyFile", FileMode.OpenOrCreate);
+        myStream.ToString();
+    }
+
+    private static SqlXml AddRbioProtocolSlimShouldReport(SqlXml remoteReplicas)
+    { // only reports with flag --pulse-increase-leak-recall.
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(remoteReplicas.Value);
+        var nodeReader = new XmlNodeReader(xmlDoc.DocumentElement);
+
+        // SqlXml is unknown, so nodeReader should be leaked here.
+        return new SqlXml(nodeReader);
     }
 }
 
 
-public class MyFavoriteClass
+public class TakeAndDispose : IDisposable
 {
+    private bool disposedValue;
+    private Stream stream;
 
+    public TakeAndDispose(Stream stream)
+    {
+        this.disposedValue = false;
+        this.stream = stream;
+    }
+
+    public virtual void Dispose()
+    {
+        if (!disposedValue)
+        {
+           
+                stream.Dispose ();
+            
+
+
+            disposedValue = true;
+        }
+    }
 }
 
-public class InheritTwoDisposable : MyFavoriteClass , IDisposable
+public class TakeWithoutDispose : IDisposable
 {
+    private bool disposedValue;
+    private Stream stream;
+
+    public TakeWithoutDispose(Stream stream)
+    {
+        this.disposedValue = false;
+        this.stream = stream;
+    }
+
+    public virtual void Dispose()
+    {
+        if (!disposedValue)
+        {
+            //stream.Dispose();
+            disposedValue = true;
+        }
+    }
+}
+
+public class TakeAndDisposeNotDisposable
+{
+    private Stream stream;
+
+    public TakeAndDisposeNotDisposable (Stream stream)
+    {
+        this.stream = stream;
+    }
+
     public void Dispose()
     {
-        //throw new NotImplementedException();
+        stream.Dispose();
     }
 }
 
 
+public class MultipleConstructors // TODO: leak reported here???
+{
+    private readonly FileStream _myStream;
+
+    public MultipleConstructors(String filename, Boolean c) : this(new FileStream(filename, FileMode.Open), c)
+    { // should report, as if c=true, we the FileStream is never stored.
+
+    }
+
+    public MultipleConstructors(FileStream myStream, Boolean c)
+    {
+        if (c)
+        {
+            throw new ArgumentException("Just because");
+        }
+
+        _myStream = myStream;
+    }
+
+    public MultipleConstructors(String filename) : this(new FileStream(filename, FileMode.Open))
+    { // should report as the FileStream is never stored.
+
+    }
+
+    public MultipleConstructors(FileStream myStream)
+    {
+        throw new ArgumentException("Just because");
+
+        _myStream = myStream;
+    }
+}
 
 
-// Should get 7 reports
+internal class ArrayTest
+{
+    public static readonly int ArraySize = 5;
+
+    public static void straightLineCodeOk()
+    {
+        FileStream[] streams = new FileStream[ArraySize];
+
+        streams[0] = new FileStream("hi", FileMode.Create);
+
+        streams[0].Close();
+    }
+
+    public static FileStream[] straightLineCodeReturnedOk ()
+    {
+        FileStream[] streams = new FileStream[ArraySize];
+
+        streams[0] = new FileStream("hi", FileMode.Create);
+
+        return streams;
+    }
+
+    public static void straightLineCodeWithVariantOk()
+    {
+        FileStream[] streams = new FileStream[ArraySize];
+
+        FileStream stream = new FileStream("hi", FileMode.Create);
+        streams[0] = stream;
+
+        stream.Close();
+    }
+
+    public static void LoopingCodeOk ()
+    {
+        FileStream[] streams = new FileStream[ArraySize];
+
+        for (int i = 0; i < ArraySize; i++)
+        {
+            streams[i] = new FileStream("hi", FileMode.Create);
+        }
+
+        for (int i = 0; i < ArraySize; i++)
+        {
+            streams[i].Close();
+        }
+    }
+
+    public static FileStream[] LoopingCodeReturnedOk ()
+    {
+        FileStream[] streams = new FileStream[ArraySize];
+
+        for (int i = 0; i < ArraySize; i++)
+        {
+            streams[i] = new FileStream("hi", FileMode.Create);
+        }
+
+        return streams;
+    }
+}
+
+internal class ListTest
+{
+    private readonly List<FileStream> _streamList = new List<FileStream>();
+    private readonly Dictionary<String, FileStream> _streamDict = new Dictionary<string, FileStream>();
+
+    public static readonly int ListSize = 5;
+
+    public static void straightLineCodeOk()
+    { // reports as no model for ElementAt().
+        List<FileStream> streams = new List<FileStream>();
+
+        streams.Add(new FileStream("hi", FileMode.Create));
+
+        streams.ElementAt(0).Close();
+    }
+
+    public static List<FileStream> straightLineCodeReturnedOk()
+    {
+        List<FileStream> streams = new List<FileStream>();
+
+        streams.Add(new FileStream("hi", FileMode.Create));
+
+        return streams;
+    }
+
+    public static void straightLineCodeWithVariantOk()
+    {
+        List<FileStream> streams = new List<FileStream>();
+
+        FileStream stream = new FileStream("hi", FileMode.Create);
+        streams.Add(stream);
+
+        stream.Close();
+    }
+
+    public static void LoopingCodeOk()
+    { // reports as no model for ElementAt()
+        List<FileStream> streams = new List<FileStream>();
+
+        for (int i = 0; i < ListSize; i++)
+        {
+            streams.Add(new FileStream("hi", FileMode.Create));
+        }
+
+        for (int i = 0; i < ListSize; i++)
+        {
+            streams.ElementAt(i).Close();
+        }
+    }
+
+    public static List<FileStream> LoopingCodeReturnedOk()
+    {
+        List<FileStream> streams = new List<FileStream>();
+
+        for (int i = 0; i < ListSize; i++)
+        {
+            streams.Add(new FileStream("hi", FileMode.Create));
+        }
+
+        return streams;
+    }
+    public void AddToFieldOk()
+    {
+        FileStream stream = new FileStream("hi", FileMode.Create);
+
+        _streamList.Add(stream);
+
+        FileStream stream2 = new FileStream("hi", FileMode.Create);
+
+        _streamDict.Add("mykey", stream2);
+    }
+
+    public async Task AddToFieldAsyncOk()
+    {
+        FileStream stream = new FileStream("hi", FileMode.Create);
+
+        _streamList.Add(stream);
+
+        FileStream stream2 = new FileStream("hi", FileMode.Create);
+
+        _streamDict.Add("mykey", stream2);
+    }
+}
