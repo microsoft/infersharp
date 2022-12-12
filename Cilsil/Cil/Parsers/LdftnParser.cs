9f94198f99be4cb26b4ccaada95d019dc50a1af3
Matthew Jin <matjin@microsoft.com>
2022-12-12 14:57:55 -0800

    progress

---
+++ Cilsil/Cil/Parsers/LdftnParser.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using Cilsil.Sil;
+using Cilsil.Sil.Expressions;
+using Cilsil.Sil.Types;
+using Cilsil.Utils;
+using Mono.Cecil.Cil;
+
+namespace Cilsil.Cil.Parsers
+{
+    internal class LdftnParser : InstructionParser
+    {
+        protected override bool ParseCilInstructionInternal(Instruction instruction,
+                                                            ProgramState state)
+        {
+            switch (instruction.OpCode.Code)
+            {
+                // Treat it like new object creation, create type of System.Intptr
+                // seems like it usually gets followed by a newobj command.
+                // In the event that this gets followed by a "calli" command, the function this refers to gets invoked
+                // TODO: should later add case Code.Ldvirtftn.
+                case Code.Ldftn:
+                    var method = instruction.Operand as Mono.Cecil.MethodReference;
+                    var funcExp = new ConstExpression(new ProcedureName(method));
+                    var funcPointer = new Tptr(Tptr.PtrKind.Pk_pointer, new Tfun());
+                    state.PushExpr(funcExp, funcPointer);
+                    state.PushInstruction(instruction.Next);
+                    return false;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ Cilsil/Sil/Types/Tfun.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace Cilsil.Sil.Types
+{
+    /// <summary>
+    /// Represents to a function type.
+    /// </summary>
+    [JsonObject]
+    public class Tfun : Typ
+    {
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this instance.
+        /// </returns>
+        public override string ToString() => "_fun_";
+        
+        /// <summary>
+        /// Determines whether the specified <see cref="object" />, is equal to this 
+        /// instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="object" /> to compare with this 
+        /// instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="object" /> is equal to this instance; 
+        ///   otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj) => obj is Tfun;
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data 
+        /// structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode() => ToString().GetHashCode();
+    }
+}
