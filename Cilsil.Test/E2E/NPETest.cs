aad9fad4af70c4919ea4cbb81c442a4df96d4798
Matthew Jin <matjin@microsoft.com>
2022-12-15 20:03:52 -0800

    bug fix

--- Cilsil.Test/E2E/NPETest.cs
+++ Cilsil.Test/E2E/NPETest.cs
@@ -838,7 +838,7 @@ namespace Cilsil.Test.E2E
         {
             TestRunManager.Run(CallTestClassMethod(
                                    TestClassMethod.NestedExceptionConditionalNullDeref,
-                                   false,
+                                   true,
                                    args: new string[]
                                    {
                                        doNullDeref.ToString()
